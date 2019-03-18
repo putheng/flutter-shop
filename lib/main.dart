d762334be7c80cbc5dc775004c1b6947e36e8c20
Putheng <puthengemail@gmail.com>
2019-03-18 16:20:22 +0700

    first init

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+
+}
+
+class _MyAppState extends State<MyApp>{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Column(
+            children: <Widget>[
+              
+            ],
+          ),
+      ),
+    );
+  }
+}
---
+++ lib/product_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shop/products.dart';
+
+class ProductManager extends StatefulWidget {
+  @override
+  _ProductManagerState createState() => _ProductManagerState();
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {
+                _products.add('Advance Food Tester');
+              });
+            },
+            child: Text('Add Produc'),
+          ),
+        ),
+        Products(_products)
+      ],
+    );
+  }
+}
