cb00683e247d2fab4438148d0277742442ae903e
Putheng <puthengemail@gmail.com>
2019-03-18 16:33:58 +0700

    lession 2.20

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:shop/products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
   _ProductManagerState createState() => _ProductManagerState();
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
