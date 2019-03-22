53d9636ead099bad19f038925453138a61b392e2
Putheng <puthengemail@gmail.com>
2019-03-22 09:30:04 +0700

    lession 6.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop/product_manager.dart';
+import 'package:shop/pages/home.dart';
 
 main() {
   runApp(MyApp());
@@ -14,12 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: ProductManager(),
-      ),
+      home: HomePage(),
     );
   }
 }
