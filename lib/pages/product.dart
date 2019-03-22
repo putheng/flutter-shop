8112c8c8b810cca41902d2ba8697239fedc5d87a
Putheng <puthengemail@gmail.com>
2019-03-22 15:38:02 +0700

    lession 6.8 Adding Reactions Upon Button Pressing

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
-
   final String title;
   final String imageUrl;
 
@@ -9,28 +9,31 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail'),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(title)
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true),
-              ),
-            )
-          ],
+    return WillPopScope(
+      onWillPop: (){
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Product Detail'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(padding: EdgeInsets.all(10.0), child: Text(title)),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('DELETE'),
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
