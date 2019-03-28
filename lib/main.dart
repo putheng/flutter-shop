d2c76bd0f49b12ee9f0f811cfd6a61ecc36dfe11
Putheng <puthengemail@gmail.com>
2019-03-28 09:23:01 +0700

    lession 6.17 parsing route data manually

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/pages/auth.dart';
+import 'package:shop/pages/product.dart';
 import 'package:shop/pages/products.dart';
 import 'package:shop/pages/products_admin.dart';
 
@@ -20,6 +21,24 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (BuildContext context) => ProductsPage(),
         '/admin': (BuildContext context) => ProductsAdminPage()
+      },
+      onGenerateRoute: (RouteSettings settings){
+        final List<String> pathElement = settings.name.split('/');
+        if(pathElement[0] != ''){
+          return null;
+        }
+        
+        if(pathElement[1] == 'product'){
+          final int index = int.parse(pathElement[2]);
+          return MaterialPageRoute(
+            builder: (BuildContext context) => ProductPage(
+              products[index]['title'],
+              products[index]['image']
+            )
+          );
+        }
+
+        return null;
       },
     );
   }
