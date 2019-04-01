4802fa93bc808e2581deb27bab16b1a79f3f6ef0
Putheng <puthengemail@gmail.com>
2019-04-01 16:37:57 +0700

    lession 8.16 refactoring code

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:shop/widgets/price_tag.dart';
+
+class ProductCard extends StatelessWidget {
+
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(
+          children: <Widget>[
+            Image.asset(product['image']),
+            Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    product['title'],
+                    style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Oswald'
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  PriceTag(
+                    '\$'+ product['price'].toString(),
+                  )
+                ],
+              )
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.info),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () => Navigator
+                  .pushNamed<bool>(
+                    context, '/product/' + productIndex.toString()
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  color: Colors.red,
+                  onPressed: () => Navigator
+                  .pushNamed<bool>(
+                    context, '/product/' + productIndex.toString()
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+  }
+}
--- lib/widgets/products.dart
+++ lib/widgets/products.dart
@@ -1,67 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:shop/widgets/price_tag.dart';
+import 'package:shop/widgets/product_card.dart';
 
 class Products extends StatelessWidget{
   final List<Map<String, dynamic>> products;
 
   Products(this.products);
-
-  Widget _buildProductItem(BuildContext context, int index){
-    return Card(
-        child: Column(
-          children: <Widget>[
-            Image.asset(products[index]['image']),
-            Container(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    products[index]['title'],
-                    style: TextStyle(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Oswald'
-                    ),
-                  ),
-                  SizedBox(width: 8.0,),
-                  PriceTag(
-                    '\$'+ products[index]['price'].toString(),
-                  )
-                ],
-              )
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.info),
-                  color: Theme.of(context).accentColor,
-                  onPressed: () => Navigator
-                  .pushNamed<bool>(
-                    context, '/product/' + index.toString()
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.favorite_border),
-                  color: Colors.red,
-                  onPressed: () => Navigator
-                  .pushNamed<bool>(
-                    context, '/product/' + index.toString()
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      );
-  }
   
   @override
   Widget build(BuildContext context) {
     if(products.length > 0){
       return ListView.builder(
-        itemBuilder: _buildProductItem,
+        itemBuilder: (BuildContext context, int index) => 
+        ProductCard(
+          products[index],
+          index
+        ),
         itemCount: products.length,
       );
     }else{
